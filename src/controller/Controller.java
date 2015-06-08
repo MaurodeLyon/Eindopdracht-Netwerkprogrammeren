612d03f96e8f83b0d17ad6fb13d045a5df3f364c
Arthur Brink <arthur.brink1@gmail.com>
2015-06-08 11:32:25 +0200

    shit

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,11 +1,19 @@
 package controller;
 
+import java.awt.geom.Rectangle2D;
+
+import view.GamePanel;
+
 public class Controller {
 
 	private Keyboard keyboard;
+	private GamePanel panel;
+	private boolean isHost;
 
-	public Controller() {
+	public Controller(GamePanel gamePanel,boolean isHost) {
 		keyboard = new Keyboard(this);
+		this.panel=gamePanel;
+		this.isHost = isHost;
 	}
 
 	public Keyboard getKeyboard() {
@@ -14,13 +22,30 @@ public class Controller {
 
 	public void Up() {
 		System.out.println("Up");
+		if(isHost)
+		{
+			panel.setPlayer1(new Rectangle2D.Double(30, panel.getPlayer1().getY()+1, 10, 50));
+		}
+		else
+		{
+			panel.setPlayer2( new Rectangle2D.Double(1080 - 40, panel.getPlayer2().getY()+1, 10,50));
+		}
+		
 	}
 
 	public void Down() {
 		System.out.println("Down");
+		if(isHost)
+		{
+			panel.setPlayer1(new Rectangle2D.Double(30, panel.getPlayer1().getY()-1, 10, 50));
+		}
+		else
+		{
+			panel.setPlayer2( new Rectangle2D.Double(1080 - 40, panel.getPlayer2().getY()-1, 10,50));
+		}
 	}
 
 	public void Released() {
-		System.out.println("Released");
+		//System.out.println("Released");
 	}
 }
