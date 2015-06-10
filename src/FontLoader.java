689f84fb9f80d31ce3672490539616bfe8106720
Arthur Brink <arthur.brink1@gmail.com>
2015-06-10 18:28:23 +0200

    Added Font for scores

---
+++ src/FontLoader.java
@@ -0,0 +1,30 @@
+
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FontLoader {
+	
+	public static Font customFont;
+	
+	public static void loadFont()
+	{
+		try {
+			customFont = Font.createFont(Font.TRUETYPE_FONT,FontLoader.class.getResourceAsStream("/PressStart2P.ttf")).deriveFont(20f); 
+			
+			GraphicsEnvironment ge = 
+		         GraphicsEnvironment.getLocalGraphicsEnvironment();
+		    ge.registerFont(customFont); 
+			//ge.registerFont(Font.createFont(Font.TRUETYPE_FONT,FontLoader.class.getResourceAsStream("/font/PressStart2P.ttf")));
+		} catch (IOException|FontFormatException e) {
+		     //Handle exception
+			System.out.println("OOPS,font not found!");
+		}
+
+	}
+
+}
--- src/Launch.java
+++ src/Launch.java
@@ -3,6 +3,7 @@ import view.Frame;
 public class Launch {
 
 	public static void main(String[] args) {
+		FontLoader.loadFont();
 		new Frame();
 	}
 
